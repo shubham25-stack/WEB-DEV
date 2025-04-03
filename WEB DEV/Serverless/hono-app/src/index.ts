e43363345f741095b98c6447a5d16fc0782b7af5
shubham25-stack <kumarshubham0683@gmail.com>
2025-04-04 02:14:59 +0530

    serverless backends

---
+++ WEB DEV/Serverless/hono-app/src/index.ts
@@ -0,0 +1,26 @@
+import { Hono } from 'hono'
+import { auth } from 'hono/utils/basic-auth';
+
+const app = new Hono()
+
+//hono middleware 
+async function authMiddleware(c: any, next: any){
+  if(c.req.header("Authorization")){
+    await next()
+  } else {
+    return c.next("You dont have acces");
+  }
+}
+
+app.use(authMiddleware)
+
+app.post('/', async (c) => {
+  const body = await c.req.json()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
+  return c.text('Hello Hono!')
+})
+
+export default app
---
+++ WEB DEV/Serverless/my-app/src/index.ts
@@ -0,0 +1,32 @@
+
+export interface Env  {
+
+}
+
+// export default {
+// 	async fetch(request, env, ctx): Promise<Response> {
+// 		return new Response('Hello World!');
+// 	},
+// } satisfies ExportedHandler<Env>;
+
+
+//http liberary use of node js
+
+export default {
+	async fetch(request: Request, env:Env,ctx:ExecutionContext):Promise <Response> {
+		console.log(request.body);
+		console.log(request.headers);
+		console.log(request.method);
+		// user,/api/v1/
+
+		if(request.method == "GET"){
+			return Response.json({
+				message:"You sent a get request"
+			});
+		} else {
+			return Response.json({
+				message: "You did not send a ger request"
+			});
+		}
+	},
+};
